038f2ec673fa0a5af8fc831dac5ae25f65f0e7a7
carlos-o <30291470+carlos-o@users.noreply.github.com>
2018-01-05 09:54:28 -0400

    campos faltantes para agregar en los serializadores de usuarios

--- account/validations.py
+++ account/validations.py
@@ -25,7 +25,9 @@ class UserSerializers(serializers.ModelSerializer):
     def create(self, validated_data):
         user = accounts_models.User(
             email=validated_data['email'],
-            username=validated_data['username']
+            username=validated_data['username'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name']
         )
         user.set_password(validated_data['password'])
         user.is_active = False
