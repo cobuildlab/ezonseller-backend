870b5056ed9b09fc61f53248462184a8c5122375
carlos-o <carlosolivero2@gmail.com>
2018-01-08 16:17:00 -0400

    fix serializer user password

--- account/validations.py
+++ account/validations.py
@@ -29,7 +29,6 @@ class UserSerializers(serializers.ModelSerializer):
             first_name = validated_data['first_name'],
             last_name = validated_data['last_name']
         )
-        user.set_password(validated_data['password'])
         user.is_active = False
         user.save()
         return user
