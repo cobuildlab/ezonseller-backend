f81b522bf8a38ad3d2dcb4cbff07f2d1274fa337
carlos <carlosolivero2@gmail.com>
2018-01-08 23:05:08 -0400

    fix error in store password, validation accounts

--- account/validations.py
+++ account/validations.py
@@ -10,25 +10,26 @@ class UserSerializers(serializers.ModelSerializer):
     email = serializers.CharField(required=True, max_length=50)
     first_name = serializers.CharField(min_length=3, max_length=30)
     last_name = serializers.CharField(min_length=3, max_length=30)
-    password = serializers.CharField(required=True, write_only=True, min_length=6, max_length=12)
+    password = serializers.CharField(default="", write_only=True, min_length=6, max_length=12)
     photo = serializers.SerializerMethodField('get_photo_url')
 
     class Meta:
         model = accounts_models.User
-        fields = ('username','first_name', 'last_name', 'email', 'photo','type')
+        fields = ('username','password','first_name', 'last_name', 'email', 'photo','type')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'username': {'read_only': True}
+        }
 
     def get_photo_url(self, obj):
         request = self.context.get('request')
         photo = obj.photo.url
         return request.build_absolute_uri(photo)
-
+    
     def create(self, validated_data):
-        user = accounts_models.User(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            first_name = validated_data['first_name'],
-            last_name = validated_data['last_name']
-        )
+        password = validated_data.get('password')
+        validated_data['password'] = make_password(password)
+        user = accounts_models.User(**validated_data)
         user.is_active = False
         user.save()
         return user
@@ -40,19 +41,21 @@ class UserSerializers(serializers.ModelSerializer):
             instance.last_name = validated_data.get('last_name')
         if validated_data.get('username'):
             instance.username = validated_data.get('username')
+        if validated_data.get('password'):
+            instance.password = make_password(validated_data.get('password'))
         return instance
 
 class UserCreateSerializers(UserSerializers):
-
-    def validate_username(self, username):
-        if accounts_models.User.objects.filter(username=username).exists():
-            raise serializers.ValidationError(u"the username already exist, please try with another username")
-        return username
 
     def validate_password(self, password):
         if not re.match(r'(?=.*[A-Za-z]+)(?=.*\d+)', password):
             raise serializers.ValidationError(_(u"the password requires characters and number"))
         return password
+
+    def validate_username(self, username):
+        if accounts_models.User.objects.filter(username=username).exists():
+            raise serializers.ValidationError(u"the username already exist, please try with another username")
+        return username
 
     def validate_email(self, email):
         if not re.search('(\w+[.|\w])*@(\w+[.])*\w+', email):
