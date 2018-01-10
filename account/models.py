33e2d3b4d3b135a11a8173030555686fb292fc03
carlos-o <carlosolivero2@gmail.com>
2018-01-10 10:22:04 -0400

    add models to payment app

--- account/models.py
+++ account/models.py
@@ -2,23 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
-USER_ADMIN = "adm"
-USER_NORMAL = "nor"
+USER_ADMIN = "admin"
+USER_NORMAL = "normal"
 TYPE_USER = (
     (USER_ADMIN, _('Admin')),
     (USER_NORMAL, _('Normal')),
 )
 
-PLAN_FREE = "free"
-PLAN_VIP = "vip"
-TYPE_PLAN= (
-    (PLAN_FREE, _('Free')),
-    (PLAN_VIP,_('Vip')),
-)
 
 class User(AbstractUser):
     type = models.CharField(_('Type'), choices=TYPE_USER, max_length=20, default=USER_NORMAL)
-    type_plan = models.CharField(_('Type_plan'), choices=TYPE_PLAN, max_length=20, default=PLAN_FREE)
     recovery = models.CharField(max_length=40, blank=True)
     photo = models.ImageField(_('Photo'), blank=True, null=True)
 
--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from payment.models import PlanSubscription, PlanSubscriptionList, TermsCondition, CreditCard
 
-# Register your models here.
+
+class CreditCardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'number_card', 'type_card', 'date_expiration')
+    search_fields = ('name',)
+    list_filter = ('type_card',)
+
+
+class TermsConditionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'created')
+    list_filter = ('created',)
+
+
+class PlanSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'type_plan', 'created', 'modified')
+    search_fields = ('title',)
+    list_filter = ('created', 'type_plan',)
+
+
+class PlanSubscriptionListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created', 'modified')
+    search_fields = ('title',)
+    list_filter = ('created',)
+
+
+admin.site.register(CreditCard, CreditCardAdmin)
+admin.site.register(TermsCondition, TermsConditionAdmin)
+admin.site.register(PlanSubscription, PlanSubscriptionAdmin)
+admin.site.register(PlanSubscriptionList, PlanSubscriptionListAdmin)
