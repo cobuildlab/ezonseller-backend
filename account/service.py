5aa7e8cebd08830c78e2b54b5a87c701db59cfdf
Luis Eulacio <luiseulacio1255@gmail.com>
2019-04-12 14:30:05 -0430

    Modify RegisterView

--- account/service.py
+++ account/service.py
@@ -1,7 +1,8 @@
 from payment.validations import CreditCardCreateValidations
-from payment.models import CreditCard
-from payment.views import CreditCardViewSet
-
+from payment.models import CreditCard, PlanSubscription,PaymentHistory
+from payment.views import CreditCardViewSet,PurchasePlanView,extract_date
+from datetime import datetime
+from payment import serializers
 def serialize_credit_card(request, user):
     request.data.get('card')['user'] = user
 
@@ -30,9 +31,61 @@ def create_card(card_serializer, user):
     stripe_card = CreditCardViewSet.stripe_costumer_card(CreditCardViewSet, id, user)
 
     if not stripe_card:
-        card.delete()
         user.delete()
         message = "cant not save the credit card please contact your bank"
         return {'message': message}
 
-    return {}
+    return {"card":card}
+
+def get_plan(user,plan_id):
+    try:
+        plan = PlanSubscription.objects.get(id=plan_id)
+    except PlanSubscription.DoesNotExist:
+        user.delete()
+        return {'message': 'the plan does not exist'}
+
+    return plan
+
+
+def create_payment(user,card,plan):
+
+    if user.type_plan == "Free" or user.type_plan == "free":
+        payment_info = PurchasePlanView.paymentPlanStripe(PurchasePlanView,plan, card, user)
+    else:
+        user.delete()
+        return {"message": "You already have an active plan and your account"}
+
+    if not payment_info:
+        user.delete()
+        return {'message': 'payment could not be made, please notify your bank distributor'}
+
+    user.type_plan = plan.title
+    user.id_plan = plan.id
+    user.save()
+    plan_finish = extract_date(plan.duration)
+    payment = PaymentHistory.objects.create(
+        user=user,
+        id_plan=plan.id,
+        paymentId=payment_info.get('payment_id'),
+        title=plan.title,
+        cost=plan.cost,
+        image=plan.image,
+        description=plan.description,
+        id_card=card.id,
+        name=card.first_name + card.last_name,
+        number_card=card.number_card,
+        cod_security=card.cod_security,
+        date_expiration=card.date_expiration,
+        date_start=datetime.now(),
+        date_finish=plan_finish,
+        accept=True,
+        unlimited_search=plan.unlimited_search,
+        number_search=plan.number_search,
+        automatic_payment=plan.automatic_payment
+    )
+
+    serializer_data = serializers.PaymentHistorySerializer(payment, many=False)
+    serializer = serializer_data.data
+
+    return {"payment_id":payment_info.get('payment_id')}
+
